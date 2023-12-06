6ecfc6407e7eba5ae6c1d96decfc860dc05d13fb
Septian Nanda Saputra <putrasanjaya498@gmail.com>
2023-12-06 17:58:28 +0700

    delete berkas

--- 1201220011_rsiyun/app/login.php
+++
@@ -1,7 +0,0 @@
-<?php
-include ("User.php");
-if (isset($_POST)) {
-    $usr = new User($_POST['username'], $_POST['password']);
-    var_dump($usr);
-}
-?>
--- 1201220012_adibf/controller.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include('User.php');
-
-$user = new User('1', "Adibfirmansyah", "adib123", "Muhammad Adib Firmansyah", '082124990753', "sidoarjo", '1');
-$user->tampilData();
